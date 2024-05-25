36fbf282764ef0bbd47aac24a952b5b453721e20
TehJunYuan <80732651+TehJunYuan@users.noreply.github.com>
2024-05-25 23:02:51 +0800

    Only Fetch Personal Edit History

--- lib/actions/image.actions.ts
+++ lib/actions/image.actions.ts
@@ -92,85 +92,61 @@ export async function getImageById(imageId: string) {
   }
 }
 
-// GET IMAGES
-export async function getAllImages({ limit = 9, page = 1, searchQuery = '' }: {
+export async function getImages({
+  limit = 9,
+  page = 1,
+  searchQuery = '',
+  userId = null,
+}: {
   limit?: number;
   page: number;
   searchQuery?: string;
+  userId?: string | null;
 }) {
   try {
     await connectToDatabase();
 
-    cloudinary.config({
-      cloud_name: process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME,
-      api_key: process.env.CLOUDINARY_API_KEY,
-      api_secret: process.env.CLOUDINARY_API_SECRET,
-      secure: true,
-    })
-
-    let expression = 'folder=imaginify';
-
-    if (searchQuery) {
-      expression += ` AND ${searchQuery}`
-    }
-
-    const { resources } = await cloudinary.search
-      .expression(expression)
-      .execute();
-
-    const resourceIds = resources.map((resource: any) => resource.public_id);
-
     let query = {};
 
-    if(searchQuery) {
-      query = {
-        publicId: {
-          $in: resourceIds
-        }
+    if (userId) {
+      query = { author: userId };
+    } else {
+      cloudinary.config({
+        cloud_name: process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME,
+        api_key: process.env.CLOUDINARY_API_KEY,
+        api_secret: process.env.CLOUDINARY_API_SECRET,
+        secure: true,
+      });
+
+      let expression = 'folder=imaginify';
+
+      if (searchQuery) {
+        expression += ` AND ${searchQuery}`;
+      }
+
+      const { resources } = await cloudinary.search
+        .expression(expression)
+        .execute();
+
+      const resourceIds = resources.map((resource: any) => resource.public_id);
+
+      if (searchQuery) {
+        query = {
+          publicId: {
+            $in: resourceIds,
+          },
+        };
       }
     }
 
-    const skipAmount = (Number(page) -1) * limit;
+    const skipAmount = (Number(page) - 1) * limit;
 
     const images = await populateUser(Image.find(query))
       .sort({ updatedAt: -1 })
       .skip(skipAmount)
       .limit(limit);
-    
+
     const totalImages = await Image.find(query).countDocuments();
-    const savedImages = await Image.find().countDocuments();
-
-    return {
-      data: JSON.parse(JSON.stringify(images)),
-      totalPage: Math.ceil(totalImages / limit),
-      savedImages,
-    }
-  } catch (error) {
-    handleError(error)
-  }
-}
-
-// GET IMAGES BY USER
-export async function getUserImages({
-  limit = 9,
-  page = 1,
-  userId,
-}: {
-  limit?: number;
-  page: number;
-  userId: string;
-}) {
-  try {
-    await connectToDatabase();
-
-    const skipAmount = (Number(page) - 1) * limit;
-
-    const images = await populateUser(Image.find({ author: userId }))
-      .sort({ updatedAt: -1 })
-      .skip(skipAmount)
-      .limit(limit);
-
-    const totalImages = await Image.find({ author: userId }).countDocuments();
 
     return {
       data: JSON.parse(JSON.stringify(images)),
