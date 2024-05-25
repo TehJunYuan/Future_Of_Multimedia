5012099adc7e68b11a0089aa43bfec084ba317e7
TehJunYuan <junyuan741@gmail.com>
2024-05-25 13:22:12 +0800

    Revert "small upate"
    This reverts commit cbb4e6f9109fa7bc8fca6f91e211606dbfedab6d.

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,20 +1,19 @@
-import MobileNav from '@/components/shared/MobileNav'
 import Sidebar from '@/components/shared/Sidebar'
+import MobileNav from '@/components/shared/mobileNav'
 import { Toaster } from '@/components/ui/toaster'
+import React from 'react'
 
-const Layout = ({ children }: { children: React.ReactNode }) => {
+const Layout = ( {children}: {children: React.ReactNode}) => {
   return (
-    <main className="root">
-      <Sidebar />
-      <MobileNav />
-
-      <div className="root-container">
-        <div className="wrapper">
-          {children}
+    <main className='root'>
+      <Sidebar/>
+      <MobileNav/>
+        <div className='root-container'>
+            <div className='wrapper'>
+                {children}
+            </div>
         </div>
-      </div>
-      
-      <Toaster />
+        <Toaster/>
     </main>
   )
 }
