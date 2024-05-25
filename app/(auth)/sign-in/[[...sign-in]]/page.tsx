7eed2c84d5b87d2e018b3498f0bbeff06a07f750
TehJunYuan <junyuan741@gmail.com>
2024-05-25 15:59:55 +0800

    update to complete system

--- app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -1,11 +1,7 @@
 import { SignIn } from '@clerk/nextjs'
-import React from 'react'
 
-const SignInPage
- = () => {
-  return (
-    <SignIn/>
-  )
+const SignInPage = () => {
+  return <SignIn />
 }
 
 export default SignInPage
--- app/(auth)/sign-up/[[...sign-up]]/page.tsx
+++ app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -1,11 +1,7 @@
 import { SignUp } from '@clerk/nextjs'
-import React from 'react'
 
-const SignUpPage
- = () => {
-  return (
-    <SignUp/>
-  )
+const SignUpPage = () => {
+  return <SignUp />
 }
 
 export default SignUpPage
