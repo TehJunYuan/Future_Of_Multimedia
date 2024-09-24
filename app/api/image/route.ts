4408cfdf5a5d46b46927fee46ad471c510a23146
TehJunYuan <junyuan741@gmail.com>
2024-09-24 23:00:40 +0800

    update for image generate

---
+++ app/api/image/route.ts
@@ -0,0 +1,51 @@
+import { auth } from "@clerk/nextjs";
+import { NextResponse } from "next/server";
+import { OpenAI } from "openai";
+
+const openai = new OpenAI({
+  apiKey: process.env.OPEN_AI_KEY,
+});
+
+console.log("OpenAI Key:", process.env.OPEN_AI_KEY);
+
+export async function POST(req: Request) {
+  try {
+    const { userId } = auth();
+    const body = await req.json();
+    const { prompt, amount = "1", resolution = "512x512" } = body;
+
+    if (!userId) {
+      return new NextResponse("Unauthorized", { status: 401 });
+    }
+
+    if (!openai.apiKey) {
+      return new NextResponse("OpenAI API Key is not configured", {
+        status: 500,
+      });
+    }
+
+    if (!prompt) {
+      return new NextResponse("Prompt are required", { status: 400 });
+    }
+    if (!amount) {
+      return new NextResponse("Amount are required", { status: 400 });
+    }
+    if (!resolution) {
+      return new NextResponse("Resolution are required", { status: 400 });
+    }
+
+
+    const response = await openai.images.generate({
+      prompt,
+      n: parseInt(amount,10),
+      size: resolution,
+    });
+    console.log("OpenAI Response:", response);
+
+    return NextResponse.json(response.data);
+    
+  } catch (error) {
+    console.log("[IMAGE_ERROR]", error);
+    return new NextResponse("Internal Error", { status: 500 });
+  }
+}
